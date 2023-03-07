0aa85211d7b50b968698489e4ea9ef8cf3f668f8
Azad Ansari <79094390+AZAD-CYBER@users.noreply.github.com>
2023-03-07 17:03:49 +0530

    power_premution

--- Problem/Backtracking/permutation.py
+++ Problem/Backtracking/permutation.py
@@ -1,21 +1,37 @@
 
-def permutations(nums):
-    result=[]
+# def permutations(nums):
+#     result=[]
 
-    #base case
-    if len(nums)==1:
-        return [nums[:]]
+#     #base case
+#     if len(nums)==1:
+#         return [nums[:]]
 
-    for i in range(len(nums)):
-        n=nums.pop(0)
-        perms=permutations(nums)
-        print(perms)
-        for perm in perms:
-            perm.append(n)
+#     for i in range(len(nums)):
+#         n=nums.pop(0)
+#         perms=permutations(nums)
+#         print(perms,n)
+#         for perm in perms:
+#             perm.append(n)
             
-        result.extend(perms)
-        nums.append(n)
+#         result.extend(perms)
+        
+#         nums.append(n)
+#         print("nums",nums)
+#     return result
+
+
+def permutations(nums):
+
+    result = [[]]
+    for n in nums:
+        new_result = []
+        for perm in result:
+            for i in range(len(perm) + 1):
+                new_result.append(perm[:i] + [n] + perm[i:])
+                print(perm[:i],[n] , perm[i:])
+        result = new_result
     return result
 
-
 print(permutations([1,2,3]))
+
+
--- recursive/Power.py
+++ recursive/Power.py
@@ -4,3 +4,19 @@ def power(x,n):
     else:
         return 1
 print(power(9,2))
+
+# Third Recursion's Return:
+# 1
+
+# This returned 1 will then be multiplied by the "x" passed in the previous call i.e power(9,1).
+
+# Second Recursion's Return:
+# 9 * 1 = 9
+
+# Similarly, this returned 9 will again be multiplied by the "x" passed in its previous call i.e power(9,2).
+
+# First Recursion's Return:
+# 9 * 9 = 81
+
+# Thus the Final answer for power(9,2) is 81.
+
